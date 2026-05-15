ab52b875769b1cf6f8750032e04be68a305e51de
Manuel <josemanueltadeo2003@gmail.com>
2026-05-15 16:57:40 -0600

    Subiendo detalles-producto,lista-productos sin logica

---
+++ frontend/src/app/pages/usuario/pages/catalogo-producto/lista-producto/lista-producto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { UiNavbarComponent } from '../../../../../components/layout/navbar/navbar';
+
+@Component({
+  selector: 'app-lista-producto',
+  imports: [UiNavbarComponent],
+  templateUrl: './lista-producto.html',
+})
+export class PageListaProducto {
+
+}
