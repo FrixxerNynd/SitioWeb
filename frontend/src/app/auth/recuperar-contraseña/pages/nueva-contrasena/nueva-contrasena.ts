f58a34b4657b2741d455179a62ea91b1351e456a
Manuel <josemanueltadeo2003@gmail.com>
2026-05-15 00:24:46 -0600

    Creando interfaz sin logica nueva-contrasena

---
+++ frontend/src/app/auth/recuperar-contraseña/pages/nueva-contrasena/nueva-contrasena.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
+  templateUrl: './nueva-contrasena.html',
+})
+export class PageNuevaContrasena {
+
+}
+
