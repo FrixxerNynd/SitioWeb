3eca1f6804a674991370018e90bd402c0bb5a1cc
Manuel <josemanueltadeo2003@gmail.com>
2026-05-14 23:05:42 -0600

    Creando interfaz sin logica de datos-fiscales

---
+++ frontend/src/app/auth/pre-registro/pages/datos-fiscales/datos-fiscales.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';  
+
+@Component({
+  imports: [ReactiveFormsModule, RouterModule],
+  templateUrl: './datos-fiscales.html',
+})
+export class PageDatosFiscales {
+
+}
