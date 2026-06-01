747357c2bff683175f642337a156478311d8d7e5
FrixxerNynd <julifer2435@gmail.com>
2026-06-01 17:50:32 -0600

    Correccion de subida de credenciales privadas

--- front-end/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    apiExcelUrl: '/api-exel',
-    Authorization: '',    
-    apiCabsUrl: '/api',
-    apiMexicoUrl: '/api-mexico'
-};
--- front-end/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// front-end/src/environments/environment.ts
-export const environment = {
-    production: false,
-    apiExcelUrl: '/api-exel',
-    Authorization: 'API_6196eef5a6a5cc36c14885c19c55c81057e603c4',    
-    apiCabsUrl: 'http://localhost:5176',
-    apiMexicoUrl: '/api-mexico' 
-};
