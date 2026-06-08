615b377d66a2149795fc2c1e44f2ced54c3235e3
Manuel <josemanueltadeo2003@gmail.com>
2026-06-08 17:42:19 -0600

    🔥 Arreglando las importaciónes de componentes

--- front-end/src/app/components/shared/etiqueta/etiqueta.component.ts
+++ front-end/src/app/components/shared/etiqueta/etiqueta.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UitipografiaComponent } from '../../atoms/tipografia/tipografia.component';
-import { UiIconComponent } from '../../atoms/icono/icono.component';
+import { UitipografiaComponent } from '../tipografia/tipografia.component'; 
+import { UiIconComponent } from '../icono/icono.component'; 
 
 export type VarianteEtiqueta = 'positivo' | 'negativo' | 'neutro';
 
